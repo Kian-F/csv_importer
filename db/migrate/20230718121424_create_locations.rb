84e87bf8ba2ba6e812c004e9b403fe3dd87ca579
mahyar <mahyar.kianfar@campaigntrack.com>
2023-07-18 22:15:00 +1000

    model

--- db/migrate/20230718121424_create_locations.rb
+++ db/migrate/20230718121424_create_locations.rb
@@ -2,6 +2,8 @@ class CreateLocations < ActiveRecord::Migration[6.0]
   def change
     create_table :locations do |t|
       t.string :name
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20230718121439_create_affiliations.rb
+++ db/migrate/20230718121439_create_affiliations.rb
@@ -2,6 +2,8 @@ class CreateAffiliations < ActiveRecord::Migration[6.0]
   def change
     create_table :affiliations do |t|
       t.string :name
+
+      t.timestamps
     end
   end
 end
